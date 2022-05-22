c4ccaa9f587d789476f5b177efed850d2e557596
gokul <gk.rsjg@gmail.com>
2022-05-22 04:26:16 -0700

    Final Commit

--- src/main/java/com/xlukog/reporterserver/controller/ReportController.java
+++ src/main/java/com/xlukog/reporterserver/controller/ReportController.java
@@ -3,10 +3,8 @@ package com.xlukog.reporterserver.controller;
 import com.xlukog.reporterserver.model.Report;
 import com.xlukog.reporterserver.model.ReportDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +15,11 @@ public class ReportController {
     @GetMapping("/view/all-reports")
     public List<Report> GetAllReport(){
         return reportDao.GetAllReport();
+
+    }
+    @RequestMapping(method = RequestMethod.GET,value = "/view/admin")
+    public String Admin(){
+        return "admin.html";
 
     }
     @PostMapping("/report/save-report")
